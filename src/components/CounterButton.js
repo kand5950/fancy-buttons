28147e602a58392fe074af20a1dc653aa1d57242
Anit <anittankandasamy@gmail.com>
2022-10-31 19:46:27 -0400

    modify state of components using second value of useState:

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 function CounterButton() {
   const [clickAmount, setClickAmount] = useState(0);
+  const handleClick = () => setClickAmount(clickAmount + 1);
+  
   return (
-    <button className="CounterButton">
+    <button onclick={handleClick} className="CounterButton">
       You clicked me {clickAmount} times
     </button>
   );
--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -2,12 +2,16 @@ import { useState } from "react";
 
 function TextRepeaterButton() {
   const [repetitions, setRepetitions] = useState(1);
+  
   const textArray = [];
   for (let i = 0; i < repetitions; i++) {
     textArray.push(<span key={i}>I like this text</span>);
   }
+
+  const handleClick = () => setRepetitions(repetitions + 1);
+  
   return (
-    <button className="TextRepeaterButton">
+    <button onClick={handleClick} className="TextRepeaterButton">
       {textArray}
     </button>
   );
